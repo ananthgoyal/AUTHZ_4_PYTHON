29eb6a88053a5256b14fcc6958e2cca00c26375d
ananthgoyal <ananthgoyal@gmail.com>
2023-06-10 02:44:41 -0700

    model classes for persistent and role

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from database import Base
-from sqlalchemy import String, Boolean, Integer, Column
+from sqlalchemy import String, Boolean, Integer, Column, ARRAY
 
 class Item(Base):
     __tablename__ = 'items'
@@ -9,13 +10,26 @@ class Item(Base):
     def __repr__(self):
         return f"<Item name = {self.name} id={self.id}>"
 
-'''
+
 class Persistent(Base):
-    pass
+    createdOn = Column(datetime)
+    createdBy = Column(Integer)
+    lastModifiedOn = Column(datetime)
+    lastModifiedBy = Column(Integer)
+    version = Column(Integer)
+    effectiveFrom = Column(datetime)
+    isEnabled = Column(Boolean)
+    id = Column(Integer, primary_key=True, unique=True)
+
 
 class Role(Persistent):
-    pass
+    __tablename__ = "roles"
+    name = Column(String(255), nullable=False, unique=False)
+    permissions = Column(ARRAY(Integer))
+    tags = Column(ARRAY(String))
 
+
+'''
 class User(Persistent):
     pass
 
